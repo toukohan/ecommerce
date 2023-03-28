d0d67c4d0ff2d0167081c79fd93af6d7e22f8a63
toukohan <114520433+toukohan@users.noreply.github.com>
2023-03-28 12:37:05 +0300

    updated user controller and routes

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -12,7 +12,7 @@ export const getUsers = async (req: Request, res: Response) => {
     }
 }
 
-export const getUserById = async (req: Request, res: Response) => {
+export const getUser = async (req: Request, res: Response) => {
     const { id } = req.params;
     try {
         const response = await db.query("SELECT * FROM users WHERE id = $1", [id]);
@@ -23,3 +23,47 @@ export const getUserById = async (req: Request, res: Response) => {
         res.json({ message: "Server error" });
     }
 }
+
+export const createUser = async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+    try {
+        const response = await db.query(
+            "INSERT INTO users (email, password) VALUES ($1, $2) RETURNING *",
+            [email, password]
+        );
+        res.json(response.rows[0]);
+    }
+    catch (err) {
+        console.error(err.message);
+        res.json({ message: "Server error" });
+    }
+}
+
+export const updateUser = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const { email, password } = req.body;
+    try {
+        const response = await db.query(
+            "UPDATE users SET email = $1, password = $2 WHERE id = $3 RETURNING *",
+            [email, password, id]
+        );
+        res.json(response.rows[0]);
+    }
+    catch (err) {
+        console.error(err.message);
+        res.json({ message: "Server error" });
+    }
+}
+
+export const deleteUser = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    try {
+        const response = await db.query("DELETE FROM users WHERE id = $1", [id]);
+        res.json({ message: "User deleted" });
+    }
+    catch (err) {
+        console.error(err.message);
+        res.json({ message: "Server error" });
+    }
+};
+
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,8 +1,17 @@
 import { Router } from "express";
-import { getUsers } from "../controllers/userController";
+
+import { getUsers, getUser, createUser, updateUser, deleteUser } from "../controllers/userController";
 
 const router = Router();
 
 router.get("/", getUsers);
+
+router.get("/:id", getUser);
+
+router.post("/", createUser);
+
+router.put("/:id", updateUser);
+
+router.delete("/:id", deleteUser);
 
 export default router;
